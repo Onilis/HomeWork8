2bc2e000eb472677b79001d9cc01fd9d2a2d4181
insai <insaittlg@gmail.com>
2025-01-09 23:50:00 +0300

    Start 1

--- src/Main.java
+++ src/Main.java
@@ -15,9 +15,9 @@ public class Main {
 
 
         System.out.println("Задача № 2");
-        System.out.println(numbers[0] + ", " + numbers[1] + ", " + numbers[2]);
-        System.out.println(numbers1[0] + ", " + numbers1[1] + ", " + numbers1[2]);
-        System.out.println(arbitraryArray[0] + ", " + arbitraryArray[1] + ", " + arbitraryArray[2]);
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(numbers1));
+        System.out.println(Arrays.toString(arbitraryArray));
 
 
         System.out.println("Задача № 3");
@@ -29,10 +29,10 @@ public class Main {
         System.out.println("Задача №4");
 
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 == 0) {
+            if (numbers[i] % 2 != 0) {
                 numbers[i] += 1;
-                System.out.println(Arrays.toString(numbers));
             }
         }
+        System.out.println(Arrays.toString(numbers));
     }
 }
